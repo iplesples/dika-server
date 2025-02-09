0abe6810bfdcba9480bad68f8afb308b05616cd9
iplesples <ceo.iplesples@gmail.com>
2025-02-09 18:46:37 +0700

    fix gallery

--- src/index.js
+++ src/index.js
@@ -1,54 +1,43 @@
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
-import upload from './middleware/multer.js';
 import path from 'path';
 import { config } from 'dotenv';
+import { fileURLToPath } from 'url';
 
 // Tentukan path secara eksplisit
 config ({ path: './.env' });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send(`Server is running on port ${port}`);
-});
-
 
 // Middleware
 app.use(cors());
-app.use(express.static('public')); // Menyajikan file statis dari folder public
+app.use(express.json());
 
+// Middleware untuk menyajikan folder publik
+app.use('/gallery', express.static(path.join(__dirname, '../public/gallery')));
 
-// Endpoint untuk upload foto
-app.post('/upload', upload.single('photo'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: 'Tidak ada file yang diupload' });
-  }
-  res.json({ message: 'Foto berhasil diupload', filePath: `/gallery/${req.file.filename}` });
-});
-
-// Endpoint untuk mendapatkan semua foto
 app.get('/api/photos', (req, res) => {
-  fs.readdir('public/gallery', (err, files) => {
+  fs.readdir(path.join(__dirname, '../public/gallery'), (err, files) => {
     if (err) {
       return res.status(500).json({ error: 'Gagal membaca folder gallery' });
     }
     const filePaths = files.map(file => ({
       name: file,
-      url: `https://dika-server.vercel.app/gallery/${file}` // URL untuk mengakses gambar
+      url: `${process.env.BASE_URL || 'http://localhost:3000'}/gallery/${file}`
     }));
     res.json(filePaths);
   });
 });
 
-// Menyajikan file statis dari folder gallery
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-// Mendapatkan __dirname
-app.use('/gallery', express.static(path.join(__dirname, 'public/gallery')))
+
 
 // Jalankan server
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
